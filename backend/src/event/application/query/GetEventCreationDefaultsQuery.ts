30c702dce82e22dd6eace011132a65be002e43e0
2010089698 <2010089698@campus.ouj.ac.jp>
2025-11-01 18:03:09 +0900

    feat: add event controller with validation

---
+++ backend/src/event/application/query/GetEventCreationDefaultsQuery.ts
@@ -0,0 +1,9 @@
+export class GetEventCreationDefaultsQuery {
+  private constructor() {}
+
+  public static create(): GetEventCreationDefaultsQuery {
+    return new GetEventCreationDefaultsQuery();
+  }
+}
+
+export default GetEventCreationDefaultsQuery;
